b1d0a5126a63cd7a412dcd3aea4b8b64c237830b
jyy <1305081045@qq.com>
2023-11-21 10:36:52 +0800

    元数据管理表详情

--- src/main/java/com/atguigu/dga/governance/controller/TableMetaInfoController.java
+++ src/main/java/com/atguigu/dga/governance/controller/TableMetaInfoController.java
@@ -4,14 +4,16 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.dga.governance.bean.TableMetaForQuery;
+import com.atguigu.dga.governance.bean.TableMetaInfo;
+import com.atguigu.dga.governance.bean.TableMetaInfoExtra;
 import com.atguigu.dga.governance.bean.vo.TableMetaInfoVo;
+import com.atguigu.dga.governance.service.TableMetaInfoExtraService;
 import com.atguigu.dga.governance.service.TableMetaInfoService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +34,9 @@ public class TableMetaInfoController {
     @Autowired
     TableMetaInfoService tableMetaInfoService;
 
+    @Autowired
+    TableMetaInfoExtraService metaInfoExtraService;
+
 
     @GetMapping("/table-list")
     public String getTableMetaList(TableMetaForQuery tableMetaForQuery) {
@@ -42,6 +47,27 @@ public class TableMetaInfoController {
         jsonObject.put("list",vos);
 
         return jsonObject.toJSONString();
+    }
+
+
+    @GetMapping("/table/{id}")
+    public String getTableMeta(@PathVariable("id")String tableId) {
+        TableMetaInfo tableMetaInfo = tableMetaInfoService.getById(tableId);
+        TableMetaInfoExtra metaInfoExtra = metaInfoExtraService.getOne(new QueryWrapper<TableMetaInfoExtra>()
+                .eq("schema_name", tableMetaInfo.getSchemaName())
+                .eq("table_name", tableMetaInfo.getTableName())
+        );
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(tableMetaInfo));
+        jsonObject.put("tableMetaInfoExtra",metaInfoExtra);
+        return jsonObject.toJSONString();
+    }
+
+
+    @PostMapping("/tableExtra")
+    public String saveTableExtra(@RequestBody TableMetaInfoExtra requestMetaInfoExtra) {
+        requestMetaInfoExtra.setUpdateTime(new Date());
+        metaInfoExtraService.saveOrUpdate(requestMetaInfoExtra);
+        return "success";
     }
 
 
