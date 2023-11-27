f3fc23b9b0133b6659a26d0f4e8f35b5570501df
jyy <1305081045@qq.com>
2023-11-27 10:53:36 +0800

    治理问题数

--- src/main/java/com/atguigu/dga/governance/controller/GovernanceAssessController.java
+++ src/main/java/com/atguigu/dga/governance/controller/GovernanceAssessController.java
@@ -2,9 +2,12 @@ package com.atguigu.dga.governance.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.dga.governance.bean.GovernanceAssessDetail;
 import com.atguigu.dga.governance.bean.GovernanceAssessGlobal;
 import com.atguigu.dga.governance.bean.GovernanceAssessTecOwner;
+import com.atguigu.dga.governance.service.GovernanceAssessDetailService;
 import com.atguigu.dga.governance.service.GovernanceAssessGlobalService;
+import com.atguigu.dga.governance.service.GovernanceAssessTableService;
 import com.atguigu.dga.governance.service.GovernanceAssessTecOwnerService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +37,12 @@ public class GovernanceAssessController {
 
     @Autowired
     GovernanceAssessTecOwnerService tecOwnerService;
+
+    @Autowired
+    GovernanceAssessTableService assessTableService;
+
+    @Autowired
+    GovernanceAssessDetailService assessDetailService;
 
     @GetMapping("/globalScore")
     public String getGlobalScore() {
@@ -70,6 +79,23 @@ public class GovernanceAssessController {
         List<Map<String, Object>> maps = tecOwnerService.listMaps(queryWrapper);
 
         return JSON.toJSONString(maps);
+    }
+
+    @GetMapping("/problemNum")
+    public String getProblemNum() {
+        QueryWrapper<GovernanceAssessDetail> governanceAssessDetailQueryWrapper = new QueryWrapper<>();
+
+        governanceAssessDetailQueryWrapper.inSql("assess_date",
+                "select max(assess_date) from governance_assess_detail"
+        ).select("sum(if(governance_type = 'SPEC' and assess_score<10,1,0)) 'SPEC'",
+                "sum(if(governance_type = 'CALC' and assess_score<10,1,0)) 'CALC'",
+                "sum(if(governance_type = 'STORAGE' and assess_score<10,1,0)) 'STORAGE'",
+                "sum(if(governance_type = 'SECURITY' and assess_score<10,1,0)) 'SECURITY'",
+                "sum(if(governance_type = 'QUALITY' and assess_score<10,1,0)) 'QUALITY'"
+                );
+
+        Map<String, Object> map = assessDetailService.getMap(governanceAssessDetailQueryWrapper);
+        return JSON.toJSONString(map);
     }
 
 }
